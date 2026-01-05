0d5c8ecdbac9fdfd6b667f15601cd7e920d532c3
Shevchenko Eugene <e.sheva007@gmail.com>
2026-01-05 19:16:55 +0200

    build header

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
+import "./main.scss";
 import "./style.css";
+
+const burger = document.querySelector("#burger");
+const mobileContainer = document.querySelector("#mobile-container");
+const body = document.querySelector("body");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("is-active");
+  mobileContainer.classList.toggle("hidden");
+  body.classList.toggle("overflow-hidden");
+});
