d86f635474fc246adf7c5a703b20fbd8f1fcae7a
Shevchenko Eugene <e.sheva007@gmail.com>
2026-01-07 21:57:33 +0200

    build banner

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import "./main.scss";
 import "./style.css";
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
 
 const burger = document.querySelector("#burger");
 const mobileContainer = document.querySelector("#mobile-container");
@@ -9,4 +11,28 @@ burger.addEventListener("click", () => {
   burger.classList.toggle("is-active");
   mobileContainer.classList.toggle("hidden");
   body.classList.toggle("overflow-hidden");
+});
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  speed: 500,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+
+    renderBullet: function (index, className) {
+      return `
+            <div class="${className} flex justify-center items-center cursor-pointer">
+                <svg viewBox="0 0 14 14" class="bullet-svg">
+                    <circle class="outer" r="5" cx="50%" cy="50%" fill="transparent" stroke="var(--color-green-400)" stroke-width="2"></circle>
+                    <circle class="inner" r="5" cx="50%" cy="50%" fill="transparent" stroke="var(--color-blue-200)" stroke-width="2" stroke-dasharray="31.4" stroke-dashoffset="31.4"></circle>
+                </svg>
+            </div>`;
+    },
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
